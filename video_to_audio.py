453b32ff5497391625df1d466d2301ae9c804bb2
jalieren <goshaberishvili@yandex.ru>
2023-03-08 22:20:02 +0300

    improve
    catching error when file not found

--- video_to_audio.py
+++ video_to_audio.py
@@ -5,9 +5,12 @@ from pathlib import Path
 tprint('jalieren')
 
 print('Your video should be in the same directory as a script')
-vid_name = str(input('Your video name: '))
-video_file = Path(f'{vid_name}.mp4')
-
-video = moviepy.editor.VideoFileClip(f'{video_file}')
-audio = video.audio
-audio.write_audiofile(f'{video_file.stem}.mp3')
+try:
+    vid_name = str(input('Your video name: '))
+    video_file = Path(f'{vid_name}.mp4')
+    video = moviepy.editor.VideoFileClip(f'{video_file}')
+    audio = video.audio
+    audio.write_audiofile(f'{video_file.stem}.mp3')
+except OSError:
+    print("File not found")
+    exit()
